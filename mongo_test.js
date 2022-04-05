dc03e3b6d9068463a8164af237cff33f2524783c
Kim Carter <87247181+kmcarter2025@users.noreply.github.com>
2022-04-04 20:33:01 -0400

    ready to deploy

--- mongo_test.js
+++ mongo_test.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
 // connect to mongo
 MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
